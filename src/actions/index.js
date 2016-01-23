b5e19e72f467cc8997e4cc771f9ed2d133d6bf87
Kyle Kelley <rgbkrk@gmail.com>
2016-01-22 20:09:54 -0600

    Provide the LAUNCH_KERNEL action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,34 @@ import { getJSON } from '../api';
 
 export const actions = new Rx.Subject();
 
+import { createShellSubject, createIOPubSubject } from 'enchannel-zmq-backend';
+
+import * as uuid from 'uuid';
+import { launch } from 'spawnteract';
+
 export const readJSON = (filePath) =>
   Rx.Observable.fromPromise(getJSON(filePath))
-    .subscribe(data => actions.onNext(Object.assign({}, { type: 'READ_JSON' }, { data })));
+    .subscribe(data => actions.onNext(Object.assign({},
+                                      { type: 'READ_JSON' },
+                                      { data })));
 
 export const updateCell = (notebook, index, cell) =>
   actions.onNext(Object.assign({}, { type: 'UPDATE_CELL' }, { notebook, index, cell }));
+
+export const launchKernel = (kernelSpecName) => {
+  return launch(kernelSpecName)
+    .then(c => {
+      const kernel = c.config;
+      const spawn = c.spawn;
+      const connectionFile = c.connFile;
+      const identity = uuid.v4();
+      const channels = {
+        shell: createShellSubject(identity, kernel),
+        iopub: createIOPubSubject(identity, kernel),
+      };
+      actions.onNext(Object.assign({},
+        { type: 'LAUNCH_KERNEL' },
+        { channels, connectionFile, spawn }
+      ));
+    });
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import * as commutable from 'commutable';
 
-import { actions } from '../actions';
+import { actions, launchKernel } from '../actions';
 
 export default function createStore(initialState) {
 
@@ -10,15 +10,34 @@ export default function createStore(initialState) {
       case 'READ_JSON':
         const { data } = action;
         const fetchedNotebook = commutable.fromJS(data);
+
+        // We now have a notebook document and can determine what language to
+        // launch
+        const kernelSpecName = fetchedNotebook.getIn(['metadata', 'kernelspec', 'name']);
+        launchKernel(kernelSpecName);
+
         return Object.assign({}, state, {
           notebook: fetchedNotebook,
         });
-        return state;
       case 'UPDATE_CELL':
         const { notebook, index, cell } = action;
         const updatedNotebook = notebook.setIn(['cells', index, 'source'], cell);
         return Object.assign({}, state, {
           notebook: updatedNotebook,
+        });
+      case 'LAUNCH_KERNEL':
+        // Note: We'll want to delete c.connFile when all done
+        //       as well as
+        //       spawn.kill() when closed
+        //       In fact, if there state.channels and state.spawn existed
+        //       then we should clean them up
+        const { channels, connectionFile, spawn } = action;
+        // At this point we now have channels.shell and channels.iopub
+        // Which are subjects that can be operated on
+        return Object.assign({}, state, {
+          channels,
+          spawn,
+          connectionFile,
         });
       }
     },
