16f4f083cafd867710766aa813fcb40b83c047f7
Phil Plückthun <phil@piemapping.com>
2016-09-09 17:57:00 +0100

    Initial switch to React Mapbox GL

--- src/notebook/components/transforms/geojson.js
+++ src/notebook/components/transforms/geojson.js
@@ -1,41 +1,32 @@
 import React from 'react';
-
-const L = require('leaflet');
-
-L.Icon.Default.imagePath = '../node_modules/leaflet/dist/images/';
+import ReactMapboxGl, { GeoJSONLayer, ScaleControl, ZoomControl } from 'react-mapbox-gl';
 
 const MIMETYPE = 'application/vnd.geo+json';
+const ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
 
 export class GeoJSONTransform extends React.Component {
-  componentDidMount() {
-    const map = L.map(this.el);
-    // TODO: Determine a strategy for picking tiles
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      id: 'mapbox.light',
-    }).addTo(map);
-    try {
-      const geoJSON = this.props.data.toJS();
-      const geoJSONLayer = L.geoJson(geoJSON).addTo(map);
-      map.fitBounds(geoJSONLayer.getBounds());
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  shouldComponentUpdate() {
-    return false;
-  }
-
   render() {
+    const data = this.props.data.toJS();
+
     return (
-      <div>
-        <link rel="stylesheet" href="../node_modules/leaflet/dist/leaflet.css" />
-        <div
-          ref={(el) => { this.el = el; }}
-          style={{ height: '600px', width: '100%' }}
-        />
-      </div>
+      <ReactMapboxGl
+        style="mapbox://styles/mapbox/light-v8"
+        accessToken={ACCESS_TOKEN}
+        containerStyle={{ height: '600px', width: '100%' }}>
+
+        <ScaleControl/>
+        <ZoomControl/>
+
+        <GeoJSONLayer
+          data={data}
+          symbolLayout={{
+            "text-field": "{place}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+          }}/>
+
+      </ReactMapboxGl>
     );
   }
 }
