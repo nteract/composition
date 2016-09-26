e24763ef374996a7235c5b3451f3344b0d7cf43e
Kyle Kelley <rgbkrk@gmail.com>
2016-09-25 19:58:40 -0700

    proxy object lookup to provide default reducer

--- src/notebook/modellers/model-reducers.js
+++ src/notebook/modellers/model-reducers.js
@@ -1,9 +1,8 @@
 
-export const targetName = 'nteract-models';
-
+// Fallback model reducer that returns pure change objects
 const justChanges = (model, change) => change;
 
-export const targetModuleReducers = {
+const modelReducers = {
   // stream and direct are aliases of each other
   stream: justChanges,
   direct: justChanges,
@@ -11,3 +10,16 @@ export const targetModuleReducers = {
   'immutable-merge': (model, change) => model.merge(Immutable.fromJS(change)),
   'lodash-merge': (model, change) => _.merge({}, model, change),
 };
+
+const fallback = {
+  get: (target, name) =>
+    // NOTE: We could throw a specialized error
+    ({}.hasOwnProperty.call(target, name) ? target[name] : justChanges)
+};
+
+
+// We return all our reducers + a fallback
+const reducers = new Proxy(modelReducers, fallback);
+
+export const targetName = 'nteract-models';
+export const targetModules = reducers;
