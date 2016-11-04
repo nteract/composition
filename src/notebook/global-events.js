b3f949ab094e4ed3e42552aab47c34e0743a86e9
Safia Abdalla <safia@safia.rocks>
2016-11-04 14:38:19 -0500

    feat(app): Ask user to save before closing

--- src/notebook/global-events.js
+++ src/notebook/global-events.js
@@ -2,10 +2,28 @@ import {
   forceShutdownKernel,
 } from './kernel/shutdown';
 
-/**
-export function beforeUnload(store, dispatch, e) {
+import { save } from './actions';
+
+import { remote } from 'electron';
+const dialog = remote.dialog;
+
+export function beforeUnload(store, e) {
+  const state = store.getState();
+  const filename = state.metadata.filename;
+  const notebook = state.document.notebook;
+  dialog.showMessageBox(remote.getCurrentWindow(), {
+    type: 'question',
+    button: ['Save', 'Don\'t Save'],
+    defaultId: 0,
+    title: 'Save Notebook',
+    message: 'Save notebook before closing?',
+    detail: 'Would you like to save this notebook before closing?',
+  }, (index) => {
+    if (index === 0) {
+      store.dispatch(save(filename, notebook));
+    }
+  }); 
 }
-*/
 
 export function unload(store) {
   const state = store.getState();
@@ -18,5 +36,6 @@ export function unload(store) {
 }
 
 export function initGlobalHandlers(store) {
+  global.window.onbeforeunload = beforeUnload.bind(null, store);
   global.window.onunload = unload.bind(null, store);
 }
