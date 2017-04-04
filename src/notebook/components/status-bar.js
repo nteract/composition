745a52f6a53bd1be745a4dff54068e8caef22a3b
alexandercbooth <alexander.c.booth@gmail.com>
2017-04-04 00:00:46 -0400

    feat: starting git integration

--- src/notebook/components/status-bar.js
+++ src/notebook/components/status-bar.js
@@ -1,13 +1,21 @@
 /* @flow */
 import React from "react";
 import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
+import moment from "moment";
+import * as path from "path";
+import { remote } from "electron";
+import * as git from "git-utils";
 
 type Props = {
+  fPath: string,
   notebook: any,
   lastSaved: Date,
   kernelSpecDisplayName: string,
   executionState: string
 };
+
+let branch = "";
+let cwd = "";
 
 export default class StatusBar extends React.Component {
   props: Props;
@@ -25,16 +33,29 @@ export default class StatusBar extends React.Component {
 
   render(): ?React.Element<any> {
     const name = this.props.kernelSpecDisplayName || "Loading...";
+    const cwd = path.dirname(this.props.fPath);
+
+    git.open(cwd) && git.open(cwd).getShortHead() != null
+      ? (branch = git.open(cwd).getShortHead() + " | ")
+      : (branch = " | ");
 
     return (
       <div className="status-bar">
         <span className="pull-right">
           {this.props.lastSaved
-            ? <p> Last saved {distanceInWordsToNow(this.props.lastSaved)} </p>
-            : <p> Not saved yet </p>}
+            ? <p>
+                <span className="octicon octicon-git-branch" />{branch}
+                Last saved {moment(this.props.lastSaved).fromNow()}{" "}
+              </p>
+            : <p>
+                <span className="octicon octicon-git-branch" />{branch}
+                Not saved yet{" "}
+              </p>}
         </span>
         <span className="pull-left">
-          <p>{name} | {this.props.executionState}</p>
+          <p>
+            {name} | {this.props.executionState}
+          </p>
         </span>
       </div>
     );
