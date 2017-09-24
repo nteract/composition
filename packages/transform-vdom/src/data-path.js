8e35038bdba938354b9ad0f0d2c55a6890044658
Kyle Kelley <rgbkrk@gmail.com>
2017-09-24 09:08:34 -0700

    provide commentary on how these models work

--- packages/transform-vdom/src/data-path.js
+++ packages/transform-vdom/src/data-path.js
@@ -3,12 +3,14 @@ const DATA_PATH_PREFIX = "data-path-for-";
 const DATA_PATH_FOR_CHILDREN = "data-path-for-children";
 
 // Given a collection of attributes and a model that (hopefully) contains
-// those paths, return a new collection of attributes replaces them according
-// to the model.
+// those paths, return a new collection of attributes that replaces them
+// according to the model.
 function modelReplaceAttributes(model, obj) {
   const changes = _.reduce(
+    // Iterate over all the attributes
     obj.attributes,
     (result, value, key) => {
+      // If they match our specced data-path-for-ATTRIBUTE, extract the model value
       if (key.startsWith(DATA_PATH_PREFIX)) {
         const changeKey = key.slice(DATA_PATH_PREFIX.length);
         const path = value;
@@ -30,23 +32,27 @@ function modelReplaceAttributes(model, obj) {
     delete changes["children"];
   }
 
-  return Object.assign({}, obj, childrenChanges, { attributes: changes });
+  // We either have to do a _.merge of the objects or we pluck exactly what we
+  // need to make the structure
+  const mergedAttributes = Object.assign({}, obj.attributes, changes);
+  return Object.assign(
+    {},
+    obj,
+    { attributes: mergedAttributes },
+    { children: childrenChanges }
+  );
 }
-
-/**
 
 var attributes = {
-    'data-path-for-value': 'what',
-    'data-path-meh': 12,
-    'data-path-for-children': 'mine',
-    'value': 3
-}
+  "data-path-for-value": "what",
+  "data-path-meh": 12,
+  "data-path-for-children": "mine",
+  value: 3
+};
 
 model = {
-    'what': 40,
-    'mine': "Hear ye, hear ye"
-}
+  what: 40,
+  mine: "Hear ye, hear ye"
+};
 
-modelReplaceAttributes(model, { attributes: attributes, children: null })
-
- */
+modelReplaceAttributes(model, { attributes: attributes, children: null });
