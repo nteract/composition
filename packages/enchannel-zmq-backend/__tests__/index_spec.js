0c39349a6c5943f2d2da5faeb51d9ef81e64442c
Safia Abdalla <safia@safia.rocks>
2017-11-21 20:27:54 -0600

    Add tests for createMainChannel

--- packages/enchannel-zmq-backend/__tests__/index_spec.js
+++ packages/enchannel-zmq-backend/__tests__/index_spec.js
@@ -5,6 +5,10 @@ import {
   createShellSubject
 } from "..";
 
+import uuidv4 from "uuid/v4";
+
+import { createMainChannel } from "../src";
+
 // Solely testing the exported interface on the built ES5 JavaScript
 describe("the built version of enchannel-zmq-backend", () => {
   it("exports create helpers for control, stdin, iopub, and shell", () => {
@@ -12,5 +16,22 @@ describe("the built version of enchannel-zmq-backend", () => {
     expect(createStdinSubject).toBeDefined();
     expect(createIOPubSubject).toBeDefined();
     expect(createShellSubject).toBeDefined();
+  });
+});
+
+describe("createMainChannel", () => {
+  it("creates a multiplexed channel", () => {
+    const config = {
+      signature_scheme: "hmac-sha256",
+      key: "5ca1ab1e-c0da-aced-cafe-c0ffeefacade",
+      ip: "127.0.0.1",
+      transport: "tcp",
+      shell_port: 19009,
+      stdin_port: 19010,
+      control_port: 19011,
+      iopub_port: 19012
+    };
+    const c = createMainChannel(uuidv4(), config);
+    expect(c).toBeDefined();
   });
 });
