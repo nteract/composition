560548a02e9915e47399a18b1181893f5da4f981
Kyle Kelley <rgbkrk@gmail.com>
2017-12-01 13:54:44 -0800

    match example to vdom's README

--- packages/play.nteract.io/pages/index.js
+++ packages/play.nteract.io/pages/index.js
@@ -58,8 +58,17 @@ export default class App extends React.Component {
       kernelStatus: "provisioning",
       kernel: null,
       error: null,
-      source: `from vdom import h1
-h1('woo')`,
+      source: `from IPython.display import display
+from vdom import h1, p, img, div, b
+
+display(
+    div(
+        h1('Our Incredibly Declarative Example'),
+        p('Can you believe we wrote this ', b('in Python'), '?'),
+        img(src="https://media.giphy.com/media/xUPGcguWZHRC2HyBRS/giphy.gif"),
+        p('What will ', b('you'), ' create next?'),
+    )
+)`,
       outputs: []
     };
   }
