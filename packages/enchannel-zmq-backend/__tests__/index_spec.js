7b9b230eae3b3dd2206afe457ff23290acca32e3
Kyle Kelley <rgbkrk@gmail.com>
2017-12-10 11:07:33 -0800

    remove deprecated enchannel test

--- packages/enchannel-zmq-backend/__tests__/index_spec.js
+++ packages/enchannel-zmq-backend/__tests__/index_spec.js
@@ -13,67 +13,6 @@ import {
   createMainChannelFromSockets
 } from "../src";
 
-describe.skip("createMainChannel", () => {
-  test("pipes messages to socket appropriately", () => {
-    // outward to the socket
-    const sent = new Subject();
-    // coming from the socket
-    const received = new Subject();
-
-    const shell = Subject.create(sent, received);
-
-    const channel = createMainChannelFromChannels(
-      { shell },
-      { session: "1234", username: "jovyan" }
-    );
-
-    // We test that our socket got the formed message
-    sent.subscribe(value => {
-      expect(value).toEqual({
-        a: "b",
-        channel: "shell",
-        header: { session: "1234", username: "jovyan", msg_id: "789AB" }
-      });
-    });
-
-    channel.next({
-      a: "b",
-      channel: "shell",
-      // msg_id should stay, session should get written over
-      header: { msg_id: "789AB", session: "XYZ" }
-    });
-  });
-
-  test("routes messages appropriately", () => {
-    // outward to the socket
-    const sent = new Subject();
-    // coming from the socket
-    const received = new Subject();
-
-    const shell = Subject.create(sent, received);
-
-    const channel = createMainChannelFromChannels(
-      { shell },
-      { session: "1234", username: "jovyan" }
-    );
-
-    channel.subscribe(value => {
-      expect(value).toEqual({
-        a: "b",
-        // mostly checking that the right channel was set
-        channel: "shell",
-        header: { msg_id: "KERNEL_MESSAGE1", session: "HAPPY" }
-      });
-    });
-
-    // Live from the socket
-    received.next({
-      a: "b",
-      header: { msg_id: "KERNEL_MESSAGE1", session: "HAPPY" }
-    });
-  });
-});
-
 describe("createSocket", () => {
   test("creates a JMP socket on the channel with identity", () => {
     const config = {
