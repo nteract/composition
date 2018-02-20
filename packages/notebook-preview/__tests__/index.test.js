735dfd07a6aa5daca63be8924306af7eb8d7c2f6
Kyle Kelley <rgbkrk@gmail.com>
2018-02-20 14:10:00 -0800

    cleanupish

--- packages/notebook-preview/__tests__/index.test.js
+++ packages/notebook-preview/__tests__/index.test.js
@@ -1,20 +1,21 @@
+// @flow
 import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
+
+// In order to get reproducible snapshots we need to mock the uuid package
+jest.mock("uuid", () => {
+  let uuid: number = 1;
+  return {
+    v4: jest.fn(() => uuid++)
+  };
+});
 
 import { displayOrder, transforms } from "@nteract/transforms";
 
 import NotebookPreview from "./../src";
 
 import { dummyCommutable, dummyJSON } from "../../core/src/dummy";
-
-// In order to get reproducable snapshots we nee to mock the uuid package
-jest.mock("uuid", () => {
-  let uuid = 1;
-  return {
-    v4: jest.fn(() => uuid++)
-  };
-});
 
 describe("Notebook", () => {
   it("accepts an Immutable.List of cells", () => {
