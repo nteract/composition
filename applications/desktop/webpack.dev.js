57e6a044791f85bd4e7ca8e0ceba5bb51e4948b5
Lukas Geiger <lukas.geiger94@gmail.com>
2018-06-12 15:17:13 +0200

    desktop: Don't split webpack build in production mode
    This reduces the time between `first paint` and `DOMLoaded` from 1.69s to 1.52s and slightly reduces the overall bundle size.

--- applications/desktop/webpack.dev.js
+++ applications/desktop/webpack.dev.js
@@ -4,6 +4,21 @@ const merge = require("webpack-merge");
 const { commonMainConfig, commonRendererConfig } = require("./webpack.common");
 
 const rendererConfig = merge(commonRendererConfig, {
+  entry: {
+    vendor: [
+      "react",
+      "react-dnd",
+      "react-dnd-html5-backend",
+      "react-dom",
+      "react-redux",
+      "redux",
+      "redux-logger",
+      "redux-observable",
+      "immutable",
+      "rxjs",
+      "date-fns"
+    ]
+  },
   plugins: [
     new webpack.SourceMapDevToolPlugin({
       filename: "[name].js.map",
