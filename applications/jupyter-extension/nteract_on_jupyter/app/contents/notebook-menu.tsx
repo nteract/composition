341ff4585536720e04cd2624a0ce3fc9ad4a193a
Kyle Kelley <rgbkrk@gmail.com>
2019-02-12 08:42:37 -0800

    hook up all the actions

--- applications/jupyter-extension/nteract_on_jupyter/app/contents/notebook-menu.tsx
+++ applications/jupyter-extension/nteract_on_jupyter/app/contents/notebook-menu.tsx
@@ -1,3 +1,4 @@
+// tslint:disable:object-literal-sort-keys
 /**
  * Declarative menu to replace the rc-menu backed menu
  */
@@ -14,17 +15,47 @@ import {
   Position,
   PopoverInteractionKind
 } from "@blueprintjs/core";
-import { AppState, ContentRef } from "@nteract/core";
+import { actions, AppState, ContentRef } from "@nteract/core";
 import { Dispatch } from "redux";
 import { connect } from "react-redux";
+
+type MenuEntry = () => void | { [item: string]: MenuEntry };
 
 interface Props {
   // Since we create the mapDispatchToProps only once (? should verify this)
   // We can make a grab bag of actions nested by menu name
   // tslint:disable-next-line:react-pure-components-have-simple-attributes
   triggers: {
-    [menu: string]: {
-      [onClick: string]: () => void;
+    // each submenu has its own actions to map with
+    file: {
+      open: () => void;
+      save: () => void;
+      download: () => void;
+    };
+    edit: {
+      cutCell: () => void;
+      copyCell: () => void;
+      pasteCell: () => void;
+      cellType: {
+        markdown: () => void;
+        code: () => void;
+      };
+    };
+    view: {
+      themes: {
+        dark: () => void;
+        light: () => void;
+      };
+    };
+    cell: {
+      runAll: () => void;
+      runAllBelow: () => void;
+      newCell: {
+        markdown: () => void;
+        code: () => void;
+      };
+      clearAllOutputs: () => void;
+      unhideAllInputAndOutput: () => void;
     };
   };
 }
@@ -36,7 +67,6 @@ export class PureNotebookMenu extends React.PureComponent<Props> {
         <Popover
           position={Position.BOTTOM_LEFT}
           minimal
-          usePortal
           interactionKind={PopoverInteractionKind.HOVER}
         >
           <Button text={"File"} minimal />
@@ -62,47 +92,61 @@ export class PureNotebookMenu extends React.PureComponent<Props> {
         <Popover
           position={Position.BOTTOM_LEFT}
           minimal
-          usePortal
-          interactionKind={PopoverInteractionKind.HOVER}
+
+          // interactionKind={PopoverInteractionKind.HOVER}
         >
           <Button text={"Edit"} />
           <Menu>
             <MenuItem
               text="Cut Cell"
               icon="cut"
-              onClick={this.props.triggers.edit.cut}
+              onClick={this.props.triggers.edit.cutCell}
             />
             <MenuItem
               text="Copy Cell"
               icon="clipboard"
-              onClick={this.props.triggers.edit.copy}
+              onClick={this.props.triggers.edit.copyCell}
             />
             <MenuItem
               text="Paste Cell Below"
               icon="clipboard"
-              onClick={this.props.triggers.edit.paste}
+              onClick={this.props.triggers.edit.pasteCell}
             />
             <MenuDivider />
-            <MenuItem
-              text="Cell Type"
-              icon="code-block"
-              onClick={this.props.triggers.edit.paste}
-            />
+            <MenuItem text="Cell Type" icon="code-block">
+              <MenuItem
+                text="Code"
+                icon="code"
+                onClick={this.props.triggers.edit.cellType.code}
+              />
+              <MenuItem
+                text="Markdown"
+                icon="new-text-box"
+                onClick={this.props.triggers.edit.cellType.code}
+              />
+            </MenuItem>
+          </Menu>
+        </Popover>
+        <Popover position={Position.BOTTOM_LEFT} minimal>
+          <Button text={"View"} />
+          <Menu>
+            <MenuItem text="Themes" icon="cog">
+              <MenuItem
+                text="light"
+                icon="flash"
+                onClick={this.props.triggers.view.themes.light}
+              />
+              <MenuItem
+                text="dark"
+                icon="moon"
+                onClick={this.props.triggers.view.themes.dark}
+              />
+            </MenuItem>
           </Menu>
         </Popover>
         <Popover
           position={Position.BOTTOM_LEFT}
           minimal
-          usePortal
-          interactionKind={PopoverInteractionKind.HOVER}
-        >
-          <Button text={"View"} />
-          <Menu />
-        </Popover>
-        <Popover
-          position={Position.BOTTOM_LEFT}
-          minimal
-          usePortal
           interactionKind={PopoverInteractionKind.HOVER}
         >
           <Button text={"Cell"} />
@@ -111,7 +155,6 @@ export class PureNotebookMenu extends React.PureComponent<Props> {
         <Popover
           position={Position.BOTTOM_LEFT}
           minimal
-          usePortal
           interactionKind={PopoverInteractionKind.HOVER}
         >
           <Button text={"Runtime"} />
@@ -120,7 +163,6 @@ export class PureNotebookMenu extends React.PureComponent<Props> {
         <Popover
           position={Position.BOTTOM_LEFT}
           minimal
-          usePortal
           interactionKind={PopoverInteractionKind.HOVER}
         >
           <Button text={"Help"} />
@@ -137,19 +179,91 @@ function makeMapDispatchToProps(
 ) {
   const { contentRef } = initialProps;
 
+  const payload = { contentRef };
+
   function mapDispatchToProps(dispatch: Dispatch) {
     return {
       triggers: {
         // each submenu has its own actions to map with
         file: {
           open: () => {
-            console.log("clicky");
-            dispatch({ type: "Clicky", payload: { contentRef } });
+            // Special case -- redirect to /tree
+          },
+          save: () => {
+            dispatch(actions.save(payload));
+          },
+          download: () => {
+            dispatch(actions.downloadContent(payload));
           }
         },
         edit: {
-          cut: () => {
-            console.log("cut me");
+          cutCell: () => {
+            dispatch(actions.cutCell(payload));
+          },
+          copyCell: () => {
+            dispatch(actions.copyCell(payload));
+          },
+          pasteCell: () => {
+            dispatch(actions.pasteCell(payload));
+          },
+          cellType: {
+            markdown: () => {
+              dispatch(actions.changeCellType({ to: "markdown", contentRef }));
+            },
+            code: () => {
+              dispatch(actions.changeCellType({ to: "code", contentRef }));
+            }
+          }
+        },
+        view: {
+          themes: {
+            dark: () => {
+              console.log("go dark");
+              dispatch(actions.setTheme("dark"));
+            },
+            light: () => {
+              dispatch(actions.setTheme("light"));
+            }
+          }
+        },
+        cell: {
+          runAll: () => {
+            dispatch(actions.executeAllCells(payload));
+          },
+          runAllBelow: () => {
+            dispatch(actions.executeAllCellsBelow(payload));
+          },
+          newCell: {
+            markdown: () => {
+              dispatch(
+                actions.createCellBelow({
+                  contentRef,
+                  cellType: "markdown",
+                  source: ""
+                })
+              );
+            },
+            code: () => {
+              dispatch(
+                actions.createCellBelow({
+                  contentRef,
+                  cellType: "code",
+                  source: ""
+                })
+              );
+            }
+          },
+          clearAllOutputs: () => {
+            dispatch(actions.clearAllOutputs(payload));
+          },
+          unhideAllInputAndOutput: () => {
+            dispatch(
+              actions.unhideAll({
+                outputHidden: false,
+                inputHidden: false,
+                contentRef
+              })
+            );
           }
         }
       }
