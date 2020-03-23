b2a173bcf5601ca473703dfb80f8be3db60d3104
kshitij <2subedikshitij@gmail.com>
2020-03-23 10:09:09 +0545

    minor cleanup

--- applications/desktop/src/main/newfile-entry.ts
+++ applications/desktop/src/main/newfile-entry.ts
@@ -1,45 +1,36 @@
-//  new .ipnb file option in Right Click Menu on Linux
-
 import { dialog } from "electron";
 
-var fs = require('fs-extra');
+var fs = require("fs-extra");
 
-var home = process.env['HOME'];
-var tdir = '/Templates';
-var fileName = '/Templates/New_ipython_file.ipynb';
-var path = home+fileName;
+var home = process.env["HOME"];
+var tdir = "/Templates";
+var fileName = "/Templates/New_ipython_file.ipynb";
+var path = home + fileName;
 
-// json data
-var jsonData = '{"cells": [{"cell_type": "code","execution_count": null,"metadata": {},"outputs": [],"source": []}],"metadata": {"kernelspec": {"display_name": "Python 3","language": "python","name": "python3"}},"nbformat": 4,"nbformat_minor": 2}';
+var jsonData =
+  '{"cells": [{"cell_type": "code","execution_count": null,"metadata": {},"outputs": [],"source": []}],"metadata": {"kernelspec": {"display_name": "Python 3","language": "python","name": "python3"}},"nbformat": 4,"nbformat_minor": 2}';
 var jsonObj = JSON.parse(jsonData);
-
-// stringify JSON Object
 var jsonContent = JSON.stringify(jsonObj);
 
 export const addRightClickMenu = () => {
-	if (process.platform === "win32") {
-    	dialog.showErrorBox(
+  if (process.platform === "win32") {
+    dialog.showErrorBox(
       "This works only for Linux as of now .",
       "Other Platform will be supported in future"
     );
-  } else{
-      if (!fs.existsSync(home+tdir)){
-	  fs.mkdirSync(home+tdir);
-	  }
-	else
-	{
-		console.log(" Templates directory Exists !!");
-	}
-	console.log(path);
+  } else {
+    if (!fs.existsSync(home + tdir)) {
+      fs.mkdirSync(home + tdir);
+    }
 
-	fs.outputFile(path,jsonContent, err => {
-  	if (err) return console.error(err)
-		dialog.showMessageBox({
+    fs.outputFile(path, jsonContent, err => {
+      if (err) return console.error(err);
+      dialog.showMessageBox({
         title: "Successfully installed.",
-        message: 'You may now be able to create new .ipynb file from anywhere.',
-        detail: 'Right Click and Select Create New_ipython_file.ipynb',
+        message: "You may now be able to create new .ipynb file from anywhere.",
+        detail: "Right Click and Select Create New_ipython_file.ipynb",
         buttons: ["OK"]
-      	})
-		})
-
-}}
+      });
+    });
+  }
+};
