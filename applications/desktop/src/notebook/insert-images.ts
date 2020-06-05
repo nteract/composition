nteract/composition

commit 8def04b46002284db4ed9640996a5e07109760bf

Dropping images: using shorthand for named function arguments
if they argument key is the same as the local variable name.

Suggested by review https://github.com/nteract/nteract/pull/5113#discussion_r435614436.

applications/desktop/src/notebook/insert-images.ts
@@ -30,17 +30,17 @@ export function insertImages({
 {
   if (embedImagesInNotebook) {
     if (base64ImageSource) {
-      createMarkdownCellWithImages({imageSources: [base64ImageSource], store: store, contentRef: contentRef});
+      createMarkdownCellWithImages({imageSources: [base64ImageSource], store, contentRef});
     } else {
       for (let imagePath of imagePaths) {
         let fileExtension = path.extname(imagePath).slice(1);
-        let imageHash = fs.readFileSync(imagePath).toString('base64');
+        let base64Image = fs.readFileSync(imagePath).toString('base64');
         createCodeCellWithImageOutput({
-          base64Image: imageHash,
+          base64Image,
           imageType: `image/${fileExtension}`,
-          imagePath: imagePath,
-          store: store,
-          contentRef: contentRef
+          imagePath,
+          store,
+          contentRef
         });
       }
     }
@@ -53,23 +53,23 @@ export function insertImages({
     if (notebookDirectory) {
       if (copyImagesToNotebookDirectory) {
         imagePaths = copyImagesToDirectoryAndReturnNewPaths({
-          imagePaths: imagePaths,
+          imagePaths,
           destinationDirectory: notebookDirectory,
-          store: store,
-          contentRef: contentRef
+          store,
+          contentRef
         })
       }
 
       imagePaths = makePathsRelativeWithinDirectory({
-        imagePaths: imagePaths,
+        imagePaths,
         directory: notebookDirectory
       });
     }
 
     createMarkdownCellWithImages({
-      imageSources: imagePaths,
-      store: store,
-      contentRef: contentRef
+      imageSources,
+      store,
+      contentRef
     });
   }
 };
@@ -93,7 +93,7 @@ function createMarkdownCellWithImages({
   store.dispatch(
     actions.createCellAbove({
       cellType: "markdown",
-      contentRef: contentRef,
+      contentRef,
       cell: newCell
     })
   );
@@ -139,7 +139,7 @@ function createCodeCellWithImageOutput({
   store.dispatch(
     actions.createCellAbove({
       cellType: "code",
-      contentRef: contentRef,
+      contentRef,
       cell: newCell
     })
   );

applications/desktop/src/notebook/global-events.ts
@@ -53,12 +53,12 @@ export function onDrop(
   let embedImagesInNotebook: boolean = !(copyImagesToNotebookDirectory || linkImagesAndKeepAtOriginalPath);
 
   insertImages({
-    imagePaths: imagePaths,
-    copyImagesToNotebookDirectory: copyImagesToNotebookDirectory,
-    linkImagesAndKeepAtOriginalPath: linkImagesAndKeepAtOriginalPath,
-    embedImagesInNotebook: embedImagesInNotebook,
-    contentRef: contentRef,
-    store: store
+    imagePaths,
+    copyImagesToNotebookDirectory,
+    linkImagesAndKeepAtOriginalPath,
+    embedImagesInNotebook,
+    contentRef,
+    store
   });
 }
 
@@ -77,10 +77,10 @@ export function onPaste(
       .filter(filePath => /[\w-]+\.(png|jpg|jpeg|heic|gif|tiff)/.test(filePath));
 
     insertImages({
-      imagePaths: imagePaths,
+      imagePaths,
       embedImagesInNotebook: true,
-      contentRef: contentRef,
-      store: store
+      contentRef,
+      store
     });
 
   // Paste blob image from the clipboard.
@@ -90,10 +90,10 @@ export function onPaste(
     let base64ImageSource = clipboard.readImage().toDataURL();
 
     insertImages({
-      base64ImageSource: base64ImageSource,
+      base64ImageSource,
       embedImagesInNotebook: true,
-      contentRef: contentRef,
-      store: store
+      contentRef,
+      store
     });
   }
 }

commit fafdd6cacd0d773ee937b2a5b4eb78cf58cb7000

Dropping images: fixing named-parameter issue
introduced by
https://github.com/nteract/nteract/pull/5113/commits/8def04b46002284db4ed9640996a5e07109760bf

discovered by
https://github.com/nteract/nteract/pull/5113#issuecomment-639495467

applications/desktop/src/notebook/insert-images.ts
@@ -67,7 +67,7 @@ export function insertImages({
     }
 
     createMarkdownCellWithImages({
-      imageSources,
+      imageSources: imagePaths,
       store,
       contentRef
     });