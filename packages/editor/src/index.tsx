28d1b699b4b66cc7664e4685c404797ee80b1fbc
Silvio <silvio@tisato.me>
2020-08-27 20:41:21 +0200

    register nim mode

--- packages/editor/src/index.tsx
+++ packages/editor/src/index.tsx
@@ -241,7 +241,7 @@ export default class CodeMirrorEditor extends React.Component<
     require("codemirror/mode/rust/rust");
 
     require("./mode/ipython");
-    require("./mode/nim");
+    require("nim-codemirror-mode").register(CodeMirror);
 
     const { editorFocused, focusAbove, focusBelow } = this.props;
 
